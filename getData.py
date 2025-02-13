bcdec5a43e988f4225968bb66ea4ae471312bfc8
aalanyabe <166563983+aalanyabe@users.noreply.github.com>
2025-02-13 16:25:58 -0500

    update1

---
+++ getData.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+
+load_dotenv(".env")
+
+app = Flask(__name__)
+
+uri = os.getenv("MONGO_URI")
+port = os.getenv("PORT")
+
+#create a new cliente and conect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client["HelpDesk"]
+
+try:
+    client.admin.command('ping')
+    print("Pinged your dep")
+except Exception as e:
+    print(e)
+
+@app.route('/get_tickets',methods=['GET'])
+
+def getTickets():
+    ejemplo = list(db.tickets.find({},{"_id":0}))
+    return jsonify(ejemplo)
+
+if __name__ == "__main__":
+    app.run(port=5000)
+
+
+
